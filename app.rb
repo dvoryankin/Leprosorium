3ce5b550b1d43cde9e20993d525ee09bfd0358e1
pchellovod <pchellovod@gmail.com>
2015-10-08 17:27:04 +0500

    index: get list of posts from database

--- app.rb
+++ app.rb
@@ -30,7 +30,11 @@ configure do
 end
 
 get '/' do
-	erb :index
+	# выбираем список постов из БД
+
+  @results = @db.execute 'select * from Posts order by id desc'
+
+  erb :index
 end
 
 # обработчик get-запроса /new
