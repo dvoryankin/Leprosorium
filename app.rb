0d3e448a4de6712422923be7baf2d3071c8aa804
pchellovod <pchellovod@gmail.com>
2015-10-08 15:19:43 +0500

    Create table Posts

--- app.rb
+++ app.rb
@@ -9,6 +9,17 @@ def init_db
 end
 
 before do
+  init_db
+end
+
+configure do
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS Posts
+  (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_date DATE,
+    content TEXT
+  )'
 
 end
 
@@ -25,3 +36,6 @@ post '/new' do
 
   erb "You typed #{content}"
 end
+
+
+
