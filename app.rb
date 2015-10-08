2643d29455c5ef8411414264a4355dc9240e019f
pchellovod <pchellovod@gmail.com>
2015-10-08 16:54:24 +0500

    Add comments

--- app.rb
+++ app.rb
@@ -12,8 +12,14 @@ before do
   init_db
 end
 
+# configure вызывается каждый раз при конфигурации приложения:
+# когда изменился код программы и перезагрузилась страница
+
 configure do
+  # инициализация БД
   init_db
+
+  # создаёт таблицу, если таблица не существует
   @db.execute 'CREATE TABLE IF NOT EXISTS Posts
   (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,11 +33,19 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+# обработчик get-запроса /new
+# (браузер получает страницу с сервера)
+
 get '/new' do
   erb :new
 end
 
+# обработчик post-запроса /new
+# (браузер отправляет страницу на сервер)
+
 post '/new' do
+  #получаем переменную из post-запроса
+
 	content = params[:content]
 
   erb "You typed #{content}"
